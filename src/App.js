1fb2deaeb11a3d7ec0303f8fe3cec92484066536
tinayetsina <tinayetsina@outlook.com>
2025-03-11 23:55:36 +0100

    update, added Navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import logo from './logo.svg';
+/* eslint-disable linebreak-style */
+import { Menu } from 'antd';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import './App.css';
+
+const items = [
+  {
+    label: 'Navigation One',
+    key: 'mail',
+    icon: <MailOutlined />,
+  },
+  {
+    label: 'Navigation Two',
+    key: 'app',
+    icon: <AppstoreOutlined />,
+    disabled: true,
+  },
+  {
+    label: 'Navigation Three - Submenu',
+    key: 'SubMenu',
+    icon: <SettingOutlined />,
+    children: [
+      {
+        type: 'group',
+        label: 'Item 1',
+        children: [
+          { label: 'Option 1', key: 'setting:1' },
+          { label: 'Option 2', key: 'setting:2' },
+        ],
+      },
+      {
+        type: 'group',
+        label: 'Item 2',
+        children: [
+          { label: 'Option 3', key: 'setting:3' },
+          { label: 'Option 4', key: 'setting:4' },
+        ],
+      },
+    ],
+  },
+  {
+    key: 'alipay',
+    label: (
+      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+        Navigation Four - Link
+      </a>
+    ),
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Menu mode="horizontal" items={items} className="navbar" />
   );
 }
 
