a2f196d26cbf890a4b9a0952c0220c1e69d31a32
tinayetsina <tinayetsina@outlook.com>
2025-03-26 14:04:01 +0100

    update - Navbar completed

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import { Menu } from 'antd';
 
 const items = [
   {
-    label: 'Navigation One',
+    label: (<a href="/home">Mail</a>),
     key: 'mail',
     icon: <MailOutlined />,
   },
@@ -12,50 +12,11 @@ const items = [
     label: 'Navigation Two',
     key: 'app',
     icon: <AppstoreOutlined />,
-    disabled: true,
-  },
-  {
-    label: 'Navigation Three - Submenu',
-    key: 'SubMenu',
-    icon: <SettingOutlined />,
-    children: [
-      {
-        type: 'group',
-        label: 'Item 1',
-        children: [
-          {
-            label: 'Option 1',
-            key: 'setting:1',
-          },
-          {
-            label: 'Option 2',
-            key: 'setting:2',
-          },
-        ],
-      },
-      {
-        type: 'group',
-        label: 'Item 2',
-        children: [
-          {
-            label: 'Option 3',
-            key: 'setting:3',
-          },
-          {
-            label: 'Option 4',
-            key: 'setting:4',
-          },
-        ],
-      },
-    ],
   },
   {
     key: 'alipay',
-    label: (
-      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-        Navigation Four - Link
-      </a>
-    ),
+    label: 'ALIPAY',
+    icon: <SettingOutlined />,
   },
 ];
 const NavBar = () => {
