cd363cfe9c5ef7b292643129a2806678bf51c145
tinayetsina <tinayetsina@outlook.com>
2025-03-27 08:09:33 +0100

    update - linter errors resolved

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -3,19 +3,17 @@ import ExamCard from '../../components/examcard/ExamCard';
 import styles from './HomePage.module.css';
 
 const HomePage = () => (
-  <>
-    <Row gutter={16} className={styles.homepage}>
-      <Col span={8}>
-        <ExamCard examName="Maths" creationDate="Monda 12 Jan" />
-      </Col>
-      <Col span={8}>
-        <ExamCard examName="Physics" creationDate="Tuesday 15 March" />
-      </Col>
-      <Col span={8}>
-        <ExamCard examName="Chemistry" creationDate="Friday 12 April" />
-      </Col>
-    </Row>
-  </>
+  <Row gutter={16} className={styles.homepage}>
+    <Col span={8}>
+      <ExamCard examName="Maths" creationDate="Monda 12 Jan" />
+    </Col>
+    <Col span={8}>
+      <ExamCard examName="Physics" creationDate="Tuesday 15 March" />
+    </Col>
+    <Col span={8}>
+      <ExamCard examName="Chemistry" creationDate="Friday 12 April" />
+    </Col>
+  </Row>
 );
 
 export default HomePage;
