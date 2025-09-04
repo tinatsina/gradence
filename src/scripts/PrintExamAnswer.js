34895227e39fd8ca4eaa29b08ebd625c13ee7b7d
Tinaye - 天籁 <tinayetsina@outlook.com>
2025-09-04 09:10:01 +0000

    update - bunnyfufu

--- src/scripts/PrintExamAnswer.js
+++ src/scripts/PrintExamAnswer.js
@@ -3,7 +3,7 @@ const pdfmake = require('pdfmake');
 const docDefinition = {
   content: [
     'First paragraph',
-    'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines',
+    'Another paragraph, this  this line will be divided into at least two lines',
   ],
 };
 
